6f70a50769b88475e41854107745e4a719e55881
Puneet Warikoo <puneetwarikoo@Outlook.com>
2018-03-10 22:58:17 -0800

    Initialized the _cachekeys in constructor

--- RateLimitWebApi/Defaults/Repository/MemoryCacheRateLimitRepository.cs
+++ RateLimitWebApi/Defaults/Repository/MemoryCacheRateLimitRepository.cs
@@ -14,6 +14,7 @@ namespace WebApi.RateLimits.Defaults
         public MemoryCacheRateLimitRepository()
         {
             _cache = MemoryCache.Default;
+            _cachekeys = new List<string>();
 
         }
         public void Clear()
